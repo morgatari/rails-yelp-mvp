2ab2501d0e8f05e6b3223629c4fb876b820cba4e
Fernando Morgado <github@fernandomorgado.eu>
2017-08-09 16:46:57 +0100

    finished the exercise

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new()
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant_id = params[:restaurant_id]
+    @restaurant = Restaurant.find(@review.restaurant_id)
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant_id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  root 'restaurants#index'
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+#                Prefix Verb   URI Pattern                                       Controller#Action
+#                  root GET    /                                                 restaurants#index
+#    restaurant_reviews POST   /restaurants/:restaurant_id/reviews(.:format)     reviews#create
+# new_restaurant_review GET    /restaurants/:restaurant_id/reviews/new(.:format) reviews#new
+#           restaurants GET    /restaurants(.:format)                            restaurants#index
+#                       POST   /restaurants(.:format)                            restaurants#create
+#        new_restaurant GET    /restaurants/new(.:format)                        restaurants#new
+#       edit_restaurant GET    /restaurants/:id/edit(.:format)                   restaurants#edit
+#            restaurant GET    /restaurants/:id(.:format)                        restaurants#show
+#                       PATCH  /restaurants/:id(.:format)                        restaurants#update
+#                       PUT    /restaurants/:id(.:format)                        restaurants#update
+#                       DELETE /restaurants/:id(.:format)                        restaurants#destroy
+
 end
