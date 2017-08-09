0369c03b88579a00858499747b9f1fd6596fc23c
Fernando Morgado <github@fernandomorgado.eu>
2017-08-09 14:56:03 +0100

    added faker gem and created models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: 0..5, allow_nil: false }
+  validates :restaurant, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::TwinPeaks.location,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save
+ end
